9b748b279676b447aa30fc0a4352a77f660d1a82
lutogin <lutogin@gmail.com>
2020-03-22 23:55:32 +0200

    Add docker and add typeorm

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:12.16.1-alpine
+EXPOSE 3000
+
+WORKDIR /home/app
+
+COPY ./ /home/app
+
+RUN yarn install
+
+CMD ["yarn", "start:dev"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,36 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      synchronize: true, // DEV only, do not use on PROD!
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    // consumer.apply(LoggerMiddleware).forRoutes(ProductsController);
-    consumer
-      .apply(LoggerMiddleware)
-      .exclude({ path: 'products', method: RequestMethod.DELETE })
-      .forRoutes({ path: 'products', method: RequestMethod.GET })
+  constructor(private readonly conncetion: Connection) {
+    console.log('Connection to DB:', conncetion.isConnected);
   }
+
+  // configure(consumer: MiddlewareConsumer) {
+  //   // consumer.apply(LoggerMiddleware).forRoutes(ProductsController);
+  //   consumer
+  //     .apply(LoggerMiddleware)
+  //     .exclude({ path: 'products', method: RequestMethod.DELETE })
+  //     .forRoutes({ path: 'products', method: RequestMethod.GET })
+  // }
 }
